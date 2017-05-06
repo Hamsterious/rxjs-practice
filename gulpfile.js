e5002ca50826d0a044696112a2d218dbccefed49
MartinJHammer <martin_hammer@live.dk>
2017-05-06 17:04:56 +0200

    First Commit

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+var tsify = require("tsify");
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
+var browserSync = require('browser-sync').create();
+
+var paths = {
+    pages: ['src/*.html']
+};
+
+// use default task to launch Browsersync and watch JS files
+gulp.task('default', ['js', 'copy-html'], function() {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
+    gulp.watch("src/**/*.ts", ['js-watch']);
+    gulp.watch("src/**/*.html", ['js-watch']);
+});
+
+gulp.task("js", function() {
+    return browserify({
+            basedir: '.',
+            debug: true,
+            entries: ['src/main.ts'],
+            cache: {},
+            packageCache: {}
+        })
+        .plugin(tsify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("copy-html", function() {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest("dist"));
+});
+
+// create a task that ensures the `js` task is complete before
+// reloading browsers
+gulp.task('js-watch', ['js', 'copy-html'], function(done) {
+    browserSync.reload();
+    done();
+});
