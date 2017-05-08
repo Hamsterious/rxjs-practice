7785e0279e404ee0901471fd5d830067cc31ae4f
MartinJHammer <martin_hammer@live.dk>
2017-05-08 21:08:12 +0200

    The individual streams now work as intended.

--- src/experiments/who-to-follow/experiment.ts
+++ src/experiments/who-to-follow/experiment.ts
@@ -1,21 +1,20 @@
 import * as Rx from 'rxjs/Rx'; // Import RxJs
-import { getGitUsersAsJsonPromise, renderGitUsers } from './who-to-follow-helpers';
+import { getGitUsersAsJsonPromise } from './who-to-follow-helpers';
 
 // Get DOM elements we want to create streams from
 let refreshButton = document.querySelector('.refresh');
-let refreshNewUserButton1 = document.querySelector('.refreshNewUser1');
-let refreshNewUserButton2 = document.querySelector('.refreshNewUser2');
-let refreshNewUserButton3 = document.querySelector('.refreshNewUser3');
+let refreshNewUserButton1 = document.querySelector('.refresh-user-1');
+let refreshNewUserButton2 = document.querySelector('.refresh-user-2');
+let refreshNewUserButton3 = document.querySelector('.refresh-user-3');
 
 // Create event streams from each DOM element. In this case, events are emitted from DOM events.
-let mainRefreshStream = Rx.Observable.fromEvent(refreshButton, 'click');
+let mainRefreshStream = Rx.Observable.fromEvent(refreshButton, 'click').startWith('startup');
 let refreshUserClickStream1 = Rx.Observable.fromEvent(refreshNewUserButton1, 'click');
 let refreshUserClickStream2 = Rx.Observable.fromEvent(refreshNewUserButton2, 'click');
 let refreshUserClickStream3 = Rx.Observable.fromEvent(refreshNewUserButton3, 'click');
 
 // Create a stream of different github urls.
 let requestStream = mainRefreshStream
-    .startWith('startup click')
     .map((x) => {
         let randomOffset = Math.floor(Math.random() * 500);
         return 'https://api.github.com/users?since=' + randomOffset + '?client_id=1b0a479a222ccfa8918a&client_secret=585bf3095db791d495764f6bdb9b7a552f3e273b';
@@ -27,11 +26,18 @@ let responseStream = requestStream.flatMap(x => {
 });
 
 let createSuggestionStream = (refreshStream) => {
-    return responseStream
-    .map((x: any) => {
-        return x[Math.floor(Math.random() * x.length)]
-    })
-    .merge(mainRefreshStream.map(() => { return null; }));
+    return refreshStream.startWith('startup click')
+        .combineLatest(responseStream,             
+            function(click, listUsers) {
+                return listUsers[Math.floor(Math.random()*listUsers.length)];
+            }
+        )
+        .merge(
+            mainRefreshStream.map(function(){ 
+                return null;
+            })
+        )
+        .startWith(null);
 }
 
 var suggestionStream1 = createSuggestionStream(refreshUserClickStream1);
@@ -39,22 +45,21 @@ var suggestionStream2 = createSuggestionStream(refreshUserClickStream2);
 var suggestionStream3 = createSuggestionStream(refreshUserClickStream3);
 
 let renderSuggestion = (suggestedUser, selector) => {
-    console.log(suggestedUser, selector);
+
     let suggestionNode = document.querySelector(selector);
+    suggestionNode.innerHTML = "";
     
     if(suggestedUser === null) {
-        let e = document.createElement('span');
-        e.innerHTML = "Nothing yet...";
-        suggestionNode.appendChild(e);
+        suggestionNode.style.visibility = 'hidden';
     }
     else
     {
-
+        suggestionNode.style.visibility = 'visible';
         let e = document.createElement('div');
         e.innerHTML = 
-        `<div style="margin-bottom:50px;">
-            <h3><a href="${suggestedUser.html_url}">${suggestedUser.login}</a></h3>
+        `<div>
             <img style="height:100px;"src="${suggestedUser.avatar_url}" />
+            <a href="${suggestedUser.html_url}">${suggestedUser.login}</a>
         </div>`;
 
         suggestionNode.appendChild(e.firstChild);
@@ -72,10 +77,3 @@ suggestionStream2.subscribe((suggestedUser) => {
 suggestionStream3.subscribe((suggestedUser) => {
     renderSuggestion(suggestedUser, '.suggestion3');
 });
-
-
-
-// Foreach result in the response stream, we render the result.
-// Note x here is a json object with multiple json objects inside. 
-// Each of these json objects describes data about a github user.
-responseStream.subscribe(x => renderGitUsers(x));
--- src/experiments/who-to-follow/who-to-follow-helpers.ts
+++ src/experiments/who-to-follow/who-to-follow-helpers.ts
@@ -18,39 +18,4 @@ let getGitUsersAsJsonPromise = function(url: string): Promise<{}> {
     return thePromise;
 };
 
-// Loops through a json object of json objects.
-// Each object represents a github user.
-// The data gets turned into html, and then gets appended to the DOM.
-let renderGitUsers = (x: {}, usersToList:number = 3) => {
-
-    emptyIdContent('who-to-follow-result');
-
-    // Render the users
-    for (let index in x) {
-
-        if (x.hasOwnProperty(index)) {
-            
-            let user = x[index];
-
-            let e = document.createElement('div');
-            e.innerHTML = 
-            `<div style="margin-bottom:50px;">
-                <h3><a href="${user.html_url}">${user.login}</a></h3>
-                <img style="height:100px;"src="${user.avatar_url}" />
-            </div>`;
-        
-            document.getElementById('who-to-follow-result').appendChild(e.firstChild);
-        }
-
-        if(--usersToList === 0) break;
-    }
-}
-
-let emptyIdContent = (id: string) => {
-    let node = document.getElementById(id);
-    while (node.hasChildNodes()) {
-        node.removeChild(node.firstChild);
-    }
-}
-
-export { getGitUsersAsJsonPromise, renderGitUsers};
+export { getGitUsersAsJsonPromise };
