92ab988feb9915fdffb5be5214a4e13d0d453fc6
MartinJHammer <martin_hammer@live.dk>
2017-05-26 15:52:30 +0200

    - Added bootstrap to all experiments. - Started working on static workshop experiment.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
+// Learning RxJs experiments
 import './experiments/add-numbers/experiment';
 import './experiments/who-to-follow/experiment';
 import './experiments/logging-clicks/experiment';
+
+// Webshop experiments
+import './experiments/webshop-1-static/experiment';
+
+// Javascript patterns
 import './misc-code/observer-pattern';
 import './misc-code/iterator-pattern';
 
