1074048f3e7914443bea38674740410e8b726baa
MartinJHammer <martin_hammer@live.dk>
2017-05-30 20:58:46 +0200

    Fixed addNewProducts timeout part, and adjust notification placement.

--- src/experiments/webshop-reactive/experiment.ts
+++ src/experiments/webshop-reactive/experiment.ts
@@ -262,10 +262,7 @@ class Webshop {
             this.showProduct(x);
             removeContentFrom(".new-product-notification");
             renderElement(".new-product-notification", 0, notification);
-        },
-        () => {},
-        () => {
-            setTimeout(() => removeContentFrom(".new-product-notification"), 5000)
+            setTimeout(() => removeContentFrom(".new-product-notification"), 3000)
         });
     }
 }
